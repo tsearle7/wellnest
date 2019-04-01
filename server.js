7ddfb38ba79b83fce90af578875209b0d27faf40
Thomas Searle <tsearle@umich.edu>
2019-04-01 17:45:36 +1100

    fixed message send. still need to add socket for client side loading

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser')
 var server = app.listen(3000, () => {
 	console.log('server is running on port', server.address().port);});
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Using Mongoose to connect to MongoDB
-var mongoose = require('mongoose');
-var dbURL = 'mongodb+srv://tsearle:dbFudgey2020@wellnest-0swvy.mongodb.net/test?retryWrites=true'
-mongoose.connect(dbUrl , (err) => { 
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://tsearle:dbFudgey2020@wellnest-0swvy.mongodb.net/test?retryWrites=true';
+mongoose.connect(uri , (err) => { 
    console.log('mongodb connected',err);
 })
 var Message = mongoose.model('Message',{ name : String, message : String})
 
-// Body parser extracts the entire body portion of an incoming request stream 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-
 // Define GET Method
-
 app.get('/messages', (req,res) => {
 	Message.find({}, (err,messages) => {
 		res.send(messages);
@@ -28,11 +24,11 @@ app.get('/messages', (req,res) => {
 })
 
 // Define POST Method
-app.post('/messages', (req, res) = > {
+app.post('/messages', (req,res) => {
 	var message = new Message(req.body);
 	message.save((err) => {
 		if(err)
-			sendStatus(500);
+			res.sendStatus(500);
 		res.sendStatus(200);
 	})
 })
